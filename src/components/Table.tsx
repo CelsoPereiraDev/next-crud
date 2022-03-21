72423734fd69a630f95b2809884c84d67fce4cbe
Celso Pereira <99039824+CelsoPereiraDev@users.noreply.github.com>
2022-03-21 16:08:31 -0300

    Criação da Tabela

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -9,9 +9,9 @@ export default function Table (props:TableProps) {
     function renderHeader() {
         return (
             <tr>
-                <th>Code</th>
-                <th>Name</th>
-                <th>Age</th>
+                <th className="text-left p-4">Code</th>
+                <th className="text-left p-4">Name</th>
+                <th className="text-left p-4">Age</th>
             </tr>
         )
     }
@@ -19,18 +19,22 @@ export default function Table (props:TableProps) {
     function renderData(){
         return props.clients?.map((Client,i) => {
             return(
-                <tr key={Client.id}>
-                    <td>{Client.id}</td>
-                    <td>{Client.name}</td>
-                    <td>{Client.age}</td>
+                <tr key={Client.id} className={`${i % 2 === 0 ? 'bg-purple-200' : 'bg-purple-300'}`}>
+                    <td className="text-left p-4">{Client.id}</td>
+                    <td className="text-left p-4">{Client.name}</td>
+                    <td className="text-left p-4">{Client.age}</td>
                 </tr>
             )
         })
     }
 
     return (
-        <table>
-            <thead>
+        <table className={`
+            w-full rounded-xl overflow-hidden
+        `}>
+            <thead className={`
+            bg-gradient-to-r from-purple-700 to-purple-500 text-white
+        `}>
                 {renderHeader()}
             </thead>   
             <tbody>
